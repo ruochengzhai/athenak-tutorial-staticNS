2187da14ffebced7c42376fab730423979ea0e57
Ruocheng Zhai <101622860+ruochengzhai@users.noreply.github.com>
2026-01-22 16:22:23 -0500

    Refactor data loading and simplify code
    Refactored data loading to use numpy for efficiency and removed unnecessary code for extracting data from the file.

--- plot_rhomax_time.py
+++ plot_rhomax_time.py
@@ -2,32 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Load file and identify the last data block
-with open('../output_scratch/tov.user.hst', 'r') as f:
-    lines = f.readlines()
-
-header_1 = "# Athena++ history data"
-header_2 = "#  [1]=time      [2]=dt       [3]=rho-max    [4]=alpha-min"
-
-# Find the index of the start of the last block
-last_idx = 0
-for i in range(len(lines) - 1):
-    if header_1 in lines[i] and header_2 in lines[i+1]:
-        last_idx = i + 2
-
-# Extract columns from the last block
-time, rho_max = [], []
-for line in lines[last_idx:]:
-    if line.startswith("#") or not line.strip(): 
-        continue
-    parts = line.split()
-    time.append(float(parts[0]))
-    rho_max.append(float(parts[2]))
+data = np.genfromtxt('path/to/tov.user.hst', comments="#", usecols=(0,2)).T
+time, rho_max = data[0,:], data[1,:]
 
 # Plot the results
 plt.figure(figsize=(4, 3))
-plt.plot(time, np.array(rho_max) / 1e-3, color='blue', linewidth=1)
+plt.plot(time, rho_max / 1e-3, color='blue', linewidth=1)
 plt.xlabel(r'Time $[M_{\odot}]$', fontsize=10)
 plt.ylabel(r'$\rho_{max}\ [10^{-3}M_{\odot}^{-2}]$', fontsize=10)
-# plt.title('Maximum Density vs Time', fontsize=10)
-# plt.legend()
+plt.grid(True)
 plt.savefig("tov_rhomax_time.pdf", dpi=300, bbox_inches='tight')
